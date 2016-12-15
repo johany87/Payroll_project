dc5e1bc48d980477d5e6e6866a8ff1f74c0a5d96
CJC18 <cjc18@192.168.1.146>
2016-12-14 21:17:55 -0500

    controller employee

--- app/controllers/employees_concepts_controller.rb
+++ app/controllers/employees_concepts_controller.rb
@@ -1,20 +1,45 @@
 class EmployeesConceptsController < ApplicationController
   def index
+    @emplyee = Employee.all
   end
 
   def new
+    @employee = Employee.new
   end
 
   def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      flash[:success] = "Employee created Ok"
+      redirect_to employee_path
+    else
+      render 'new'
+    end #if
   end
 
   def destroy
+    find_employee
+    if @employee.destroy
+        flash[:success] = "Employee Destroyed Successfully"
+    else
+        flash[:error] = "Employee couldn't delete"
+    end #if
+      redirect_to employee_path
   end
 
   def edit
+    @employee = Employee.all
   end
 
   def update
+    find_employee
+    if @employee.update(employee_params)
+      flash[:success] = "Employee updated"
+      redirect_to employe_path
+    else
+      flash[:error] = "Concept couldn't updated"
+      render 'new'
+    end #if
   end
 
   def show
